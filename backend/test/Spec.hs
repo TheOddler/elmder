2e5da38eb252015486d0b7c5fcaa19e1e21d3477
Pablo Bollansée <pablo.bollansee@gmail.com>
2023-07-27 09:55:08 +0100

    WIP: Extra HContains instances

--- backend/test/Spec.hs
+++ backend/test/Spec.hs
@@ -9,7 +9,7 @@ import Data.Text qualified as T
 import Database.Postgres.Temp (toConnectionString)
 import Database.Postgres.Temp qualified as PgTemp
 import Network.HTTP.Client qualified as HTTP
-import Servant
+import Servant (Handler, HasServer (ServerT), serve)
 import Servant.Client
 import Servant.Server.Generic (AsServerT)
 import Test.QuickCheck
@@ -18,6 +18,12 @@ import Test.Syd
 import Test.Syd.Wai.Def (applicationSetupFunc, managerSpec)
 import User
 import Web
+
+instance (HContains l a, HContains l b) => HContains l (a, b) where
+  getElem l = (getElem l, getElem l)
+
+instance (HContains l a, HContains l b) => HContains l (HList '[a, b]) where
+  getElem l = HCons (getElem l) $ HCons (getElem l) $ HNil
 
 api :: ApiRoutes (AsClientT ClientM)
 api = client apiProxy
@@ -38,15 +44,16 @@ serverSetupFunc dbCache = do
       Left err -> expectationFailure $ show err
       Right a -> pure a
 
-serverTest :: TestDef '[(PgTemp.Cache, HTTP.Manager), HTTP.Manager] ClientEnv -> Spec
+serverTest :: TestDef '[PgTemp.Cache, HTTP.Manager] ClientEnv -> Spec
 serverTest =
-  let setupCacheAndManager :: HTTP.Manager -> SetupFunc (PgTemp.Cache, HTTP.Manager)
-      setupCacheAndManager man = do
-        dbCache <- dbCacheSetupFunc
-        pure (dbCache, man)
+  let setupClient :: (PgTemp.Cache, HTTP.Manager) -> SetupFunc ClientEnv
+      setupClient (dbCache, man) = serverSetupFunc dbCache >>= clientEnvSetupFunc man
+
+      setupClientHList :: HList '[PgTemp.Cache, HTTP.Manager] -> SetupFunc ClientEnv
+      setupClientHList outers = serverSetupFunc (getElem outers) >>= clientEnvSetupFunc (getElem outers)
    in managerSpec
-        . setupAroundAllWith setupCacheAndManager
-        . setupAroundWith' (\(dbCache, man) () -> serverSetupFunc dbCache >>= clientEnvSetupFunc man)
+        . setupAroundAll dbCacheSetupFunc
+        . setupAroundWith' (\outers () -> setupClientHList outers)
 
 clientEnvSetupFunc :: HTTP.Manager -> ServerT Web.Api Handler -> SetupFunc ClientEnv
 clientEnvSetupFunc man server = do
