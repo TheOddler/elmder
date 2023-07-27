d0c6ba7779f9fe784ac64e58b27151c1f2289114
Pablo Bollansée <pablo.bollansee@gmail.com>
2023-07-27 11:37:07 +0100

    Updated comment

--- backend/test/SydTestExtra.hs
+++ backend/test/SydTestExtra.hs
@@ -2,6 +2,7 @@ module SydTestExtra where
 
 import Test.Syd (HList, SetupFunc, TestDefM, setupAroundWith')
 
--- | This simply gives the compiler some more type information
+-- | Use multiple outers when creating an inner
+-- I'll be contributing this function to SydTest, but until it's been merged I'll keep this here.
 setupAroundWithAll :: (HList outers -> oldInner -> SetupFunc newInner) -> TestDefM outers newInner result -> TestDefM outers oldInner result
 setupAroundWithAll = setupAroundWith'
